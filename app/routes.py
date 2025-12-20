224f3b3ac01113ec0b4e57f16c839dc2dbcbfdbb
Ronda <johnronda0527@gmail.com>
2025-12-20 20:42:25 +0800

    added pagination to index and explore page

--- app/routes.py
+++ app/routes.py
@@ -29,9 +29,12 @@ def index():
                 db.session.commit()
                 flash('Your post is now live!')
                 return redirect(url_for('index'))
-        posts = current_user.followed_posts().all()
-        return render_template('index.html', title='Home Page', form=form, posts = posts)
-
+        page = request.args.get('page', 1, type=int)
+        posts = current_user.followed_posts().paginate(
+                page=page,
+                per_page=app.config['POSTS_PER_PAGE'],
+                error_out=False)
+        return render_template('index.html', title='Home', form=form, posts=posts.items)
 @app.route('/login', methods=['GET', 'POST'])
 def login():
         if current_user.is_authenticated:
@@ -136,8 +139,12 @@ def unfollow(username):
 @app.route('/explore')
 @login_required
 def explore():
-        posts = Post.query.order_by(Post.timestamp.desc()).all()
-        return render_template('index.html', title='Explore', posts=posts)
+        page = request.args.get('page', 1, type=int)
+        posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+                page=page,
+                per_page=app.config['POSTS_PER_PAGE'],
+                error_out=False)
+        return render_template('index.html', title='Explore', posts=posts.items)
 
 
 
