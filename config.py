cb66d0a3075935704ff61211ec34c26f7d3841aa
Ronda <johnronda0527@gmail.com>
2025-12-21 22:21:30 +0800

    added pagination links in the user page

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config(object):
         MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
         MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
         ADMINS = ['your-email@example.com']
-        POSTS_PER_PAGE =3
+        POSTS_PER_PAGE =25
 
 class TestingConfig():
         TESTING = True
