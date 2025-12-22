eac03608c4e51ff7b27370c29623cf2b1dcd4d62
Ronda <johnronda0527@gmail.com>
2025-12-22 18:35:15 +0800

    set up flask mail

--- config.py
+++ config.py
@@ -5,12 +5,18 @@ class Config(object):
         SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
         SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
         SQLALCHEMY_TRACK_MODIFICATIONS = False
-        MAIL_SERVER = os.environ.get('MAIL_SERVER')
-        MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-        MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-        MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-        MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-        ADMINS = ['your-email@example.com']
+        # MAIL_SERVER = os.environ.get('MAIL_SERVER')
+        # MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+        # MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+        # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+        # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+        # ADMINS = ['your-email@example.com']
+        MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
+        MAIL_PORT = int(os.environ.get('MAIL_PORT', 587))
+        MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true', 'on', '1']
+        MAIL_USERNAME = os.environ.get('MAIL_USERNAME', 'johnronda0527@gmail.com')
+        MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', 'hrzuvlpqkezjiceh')
+        ADMINS = ['yukinonxhachiman@gmail.com']
         POSTS_PER_PAGE =25
 
 class TestingConfig():
