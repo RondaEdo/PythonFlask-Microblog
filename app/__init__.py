998e7788670ca02342b628aa49f5b37b1836e502
Ronda <johnronda0527@gmail.com>
2025-12-27 21:56:07 +0800

    updated base.html

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message_category = 'warning'
 mail = Mail(app)
 
 from app import routes, models, errors
