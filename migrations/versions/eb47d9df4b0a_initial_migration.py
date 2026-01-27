e66ebce828e94432720903f29e78b3cf4aaff5fd
Ronda <johnronda0527@gmail.com>
2026-01-27 23:01:59 +0800

    Added language to post model
    Fixed migration error

---
+++ migrations/versions/eb47d9df4b0a_initial_migration.py
@@ -0,0 +1,24 @@
+"""initial migration
+
+Revision ID: eb47d9df4b0a
+Revises: 
+Create Date: 2026-01-27 22:55:55.680367
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'eb47d9df4b0a'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
